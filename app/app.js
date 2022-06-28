0fb0cceb83ae6762e3afa6fbbf9d8f2feb8201d3
Matteo <matteo68.mc@gmail.com>
2022-06-28 10:43:26 +0200

    Added Virus Total file upload

--- app/app.js
+++ app/app.js
@@ -9,10 +9,6 @@ app.set('view engine', 'ejs');
 
 app.use('/static', express.static(path.join(__dirname, './static/'))); 
 
-//Imposta limiti body
-app.use(parser.json({limit: '50mb'}));
-app.use(parser.urlencoded({limit: '50mb', extended: true}));
-
 app.use('/', require('./controllers/ui/index'))
 app.use('/api', require('./controllers/api/index'))
 
